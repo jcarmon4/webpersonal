43b3546d5a279da09ed51a68bb31852ae486f8a4
alejocram <alejocram@gmail.com>
2019-02-27 22:56:33 -0500

    Se crea un proyecto basico en DJango, definiendose el routing. Se crea una primera app, y se implementan metodos para las vistas.

---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Alejocram</h1>
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/portfolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contactenos</a></li>
+    <li><a href="/about/">Acerca de</a></li>
+</ul>
+"""
+
+# Create your views here.
+def home(request):
+    html_response = """
+        <h3>Home</h3>
+    """
+    return HttpResponse(html_base + html_response)
+
+def about(request):
+    html_response = """
+        <h3>Acerca de</h3>
+    """
+    return HttpResponse(html_base + html_response)
+
+def portfolio(request):
+    html_response = """
+        <h3>Portafolio</h3>
+    """
+    return HttpResponse(html_base + html_response)
+
+def contact(request):
+    html_response = """
+        <h3>Contactenos</h3>
+    """
+    return HttpResponse(html_base + html_response)
