0199137944564f137075ff30feb6f9d6e469cbe8
alejocram <alejocram@gmail.com>
2019-02-28 00:25:57 -0500

    Se modifica para utiliza URL tag en about y no tener urls quemadas

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -19,7 +19,7 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
+    path('about/', views.about, name="about-me"),
     path('portfolio/', views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
