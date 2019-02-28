72eb7279b8adbcafb15a796cc7a34baa3cf8248a
alejocram <alejocram@gmail.com>
2019-02-27 23:44:00 -0500

    Se crean los templates para about, portfolio y contact.

--- core/views.py
+++ core/views.py
@@ -1,33 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Alejocram</h1>
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contactenos</a></li>
-    <li><a href="/about/">Acerca de</a></li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
     return render(request, "core/home.html" )
 
 def about(request):
-    html_response = """
-        <h3>Acerca de</h3>
-    """
-    return HttpResponse(html_base + html_response)
+    return render(request, "core/about.html" )
 
 def portfolio(request):
-    html_response = """
-        <h3>Portafolio</h3>
-    """
-    return HttpResponse(html_base + html_response)
+    return render(request, "core/portfolio.html" )
 
 def contact(request):
-    html_response = """
-        <h3>Contactenos</h3>
-    """
-    return HttpResponse(html_base + html_response)
+    return render(request, "core/contact.html" )
