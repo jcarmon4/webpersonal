299ea19045650f2a7a301dbd2f149359a3ec1cb2
alejocram <alejocram@gmail.com>
2019-03-03 17:21:44 -0500

    Con el atributo upload_to, se creara una carpeta dentro de media para almacenar las imagenes de este campo.

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     description = models.TextField(verbose_name="Descripción")
-    image = models.ImageField(verbose_name="Imagen")
+    image = models.ImageField(verbose_name="Imagen", upload_to = "projects")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
