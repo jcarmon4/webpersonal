ba22749bd4254a516a71751e24d69bd207cade81
alejocram <alejocram@gmail.com>
2019-03-03 19:31:20 -0500

    Corrección: Se agrega el atributo link de tipo URLField al modelo, con atributos null=True para que la DB lo admita y blank=True para que permita agregar proyectos sin link.

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     description = models.TextField(verbose_name="Descripción")
     image = models.ImageField(verbose_name="Imagen", upload_to = "projects")
-    link = models.URLField(null=True, blank=False)
+    link = models.URLField(verbose_name="URL", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
